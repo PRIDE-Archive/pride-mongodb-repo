ca8987e0bc70af77571dfdccf459dc835da54b0f
Chakradhar Bandla <chakrabandla@gmail.com>
2021-09-14 01:27:27 +0100

    minor

--- src/main/java/uk/ac/ebi/pride/mongodb/molecules/model/peptide/PrideMongoPeptideSummary.java
+++ src/main/java/uk/ac/ebi/pride/mongodb/molecules/model/peptide/PrideMongoPeptideSummary.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import uk.ac.ebi.pride.mongodb.archive.model.PrideArchiveField;
